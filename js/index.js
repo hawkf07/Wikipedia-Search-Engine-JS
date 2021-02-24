f701d07c5e126cdc8aebf94b96910771f1ef3ae2
fikri212 <fikrifadilah299@gmail.com>
2021-02-24 22:34:50 +0700

    Initial Commit 🔥

---
+++ js/index.js
@@ -0,0 +1,36 @@
+let searchInput = document.querySelector("#searchInput");
+const timesIcon = document.querySelector(".fas.fa-times");
+const searchIcon = document.querySelector(".fas.fa-search");
+const wikiH1 = document.querySelector(".wiki-h1");
+const wikiResult = document.querySelector(".wiki-result");
+const wikiArticle = document.querySelector(".wiki-article");
+let wikiForm = document.querySelector(".search-input");
+const darkMode = document.querySelector(".dark-mode");
+
+darkMode.addEventListener("click", () => {
+  darkMode.classList.toggle("color-change");
+  document.body.classList.toggle("bg-change");
+});
+const wikipediaSearch = async () => {
+  let api = await fetch(
+    `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srsearch=${searchInput.value}&srlimit=15`
+  );
+  let data = await api.json();
+  console.log(data);
+  timesIcon.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+  data.query.search.forEach((result) => {
+    url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+    wikiH1.textContent = `result for ${searchInput.value}, totalhits ${data.query.searchinfo.totalhits}...`;
+    wikiArticle.insertAdjacentHTML(
+      "afterend",
+      `
+         <a href='${url}' target='blank'> ${result.title} </a>
+         <p class="snippet"> ${result.snippet} </p>
+         `
+    );
+  });
+};
+timesIcon.addEventListener("click", (e) => (searchInput.value = ""));
+searchIcon.addEventListener("click", wikipediaSearch);
